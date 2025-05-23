d69543af769c5d07e4d52d1e76f3fd199b8afbd1
Guilherme S Xavier <guisx322@gmail.com>
2025-05-22 23:13:33 -0300

    Verificar senha usuario
    função para autenticar senha do usuario cadastrado

---
+++ Projeto4C-main/ProgramaUser/FuncoesUSER/verificasenhaUsu.c
@@ -0,0 +1,13 @@
+#include "../../shared/opcoes.h"
+
+// Função para verificar a senha do usuário
+int verificarSenha(char *usuario, char *senha) {
+  char **dadosUsuario = importarInfo(usuario);
+  if (dadosUsuario != NULL) {
+    if (strcmp(senha, dadosUsuario[1]) == 0) {
+      return 1; // Senha correta
+    }
+  }
+  return 0; // Senha incorreta ou usuário não encontrado
+}
+
